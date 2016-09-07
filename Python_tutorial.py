399c237c5e07f71eee64b3a69f3a54c6058ba044
emelieahlin <emelieahlinandersson@gmail.com>
2016-09-07 15:29:24 +0200

    Gett 10 % rabatt om summan blir högre än 500 kr.
    def price_with_tax(self):
    total= self.price * self.count * self.tax
    if total > 500:
    return 0.9*total
    else:
    return total

--- Python_tutorial.py
+++ Python_tutorial.py
@@ -2,20 +2,26 @@ class Product:
 	price = 0
 	count = 0
 	tax = 0
-	def price_with_tax(self):
-		return (self.price * self.count * self.tax)
-
+	
 	def __init__(self,price,count,tax):
 		self.price = price 
 		self.count = count
 		self.tax = tax 
 
+	def price_with_tax(self):
+		total= self.price * self.count * self.tax
+		if total > 500: 
+			return 0.9*total
+		else: 
+			return total	
+
 products = [
 	Product(price=900, count=2, tax=1.25), #robot
-	Product(price=100, count=1, tax=1.06) #book
+	Product(price=100, count=1, tax=1.06), #book
+	Product(price=50, count=2, tax=1.25) #batterier
 	]
 
 total_price = 0
 for product in products: 
-		total_price += product.price_with_tax()
+	total_price += product.price_with_tax()
 print(total_price)
