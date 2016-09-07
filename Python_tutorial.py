0677b932c6d96704308f3f810b72d780ac0f7de4
emelieahlin <emelieahlinandersson@gmail.com>
2016-09-07 12:04:27 +0200

    Metod print

---
+++ Python_tutorial.py
@@ -0,0 +1,20 @@
+class Product:
+	price = 0
+	count = 0
+	tax = 0
+	def price_with_tax(self):
+		print (self.price * self.count * self.tax)
+
+robot = Product()
+robot.price = 900
+robot.count = 2
+robot.tax = 1.25
+robot.price_with_tax()
+
+book = Product()
+book.price = 100
+book.count = 1
+book.tax = 1.06
+book.price_with_tax()
+
+
